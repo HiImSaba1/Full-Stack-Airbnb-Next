8f3a4b23da8a0e6e51af4b73766a252c7f745a18
Pavlos Saba <pavlossaba@gmail.com>
2024-01-26 19:33:34 +0200

    Created Modal for user menu and working on registerModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,33 @@
-import { Nunito } from 'next/font/google'
-import Navbar from '@/app/components/navbar/Navbar';
-import './globals.css'
-
+import { Nunito } from "next/font/google";
+import Navbar from "@/app/components/navbar/Navbar";
+import "./globals.css";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="en">
       <body className={font.className}>
-          <Navbar  />
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
-  )
+  );
 }
